051fe9a2d9a82a73dc190ad7979eb1adda24b558
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2023-02-27 20:37:06 +0600

    rtk add & post

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import { productApi } from '../features/api/apiSlice.js'
 
 export const store = configureStore({
   reducer: {
+    [productApi.reducerPath]: productApi.reducer,
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware)
 });
--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,13 +1,35 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const productApi = createApi({
+export const productApi = createApi({
     reducerPath: "productApi",
     baseQuery: fetchBaseQuery({
         baseUrl: "http://localhost:5000",
     }),
     endpoints: (builder) => ({
-        getProduct: builder.query({
-            query: () => "/product",
+        getProducts: builder.query({
+            query: () => ({
+                url: "/product"
+            }),
         }),
+        addProduct: builder.mutation({
+            query: (data) => ({
+                url: "/product",
+                method: "POST",
+                body: data
+            })
+        })
     }),
 });
+
+export const { useGetProductsQuery, useAddProductMutation } = productApi;
+
+
+// the useGetProductsQuery returns an object with many things
+// const { data, isError, isLoading, isSuccess } = useGetProductsQuery();
+
+// and useAddProductMutation returns an array with two things
+// const [postProduct, result] = useAddProductMutation();
+// here postProduct is a function which is will be called at product posting time and result is an object which is same as above one
+
+// const [postProduct, { isError, isSuccess }] = useAddProductMutation();
+
